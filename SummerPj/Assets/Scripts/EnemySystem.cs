35fbb84430284499e5876075601deedc86488a5d
river0203 <aspp3666@gmail.com>
2023-07-05 17:36:19 +0900

    .

--- SummerPj/Assets/Scripts/EnemySystem.cs
+++ SummerPj/Assets/Scripts/EnemySystem.cs
@@ -29,37 +29,5 @@ public class EnemySystem : MonoBehaviour
         enemy_state = State.Run;
     }*/
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        //무기 태그와 충돌 하였을 때 발동
-        if (collision.collider.gameObject.CompareTag("Weapon"))
-        {
-            Debug.Log("onDamage");
-            hp -= damage;
-            Debug.Log(hp);
-            enemy_state = State.IsHitting;
-            if (enemy_state == State.IsHitting)
-            {
-                //play stage2 anim
-                StartCoroutine("hitting_delay");
-
-                if (hp <= 0)
-                {
-                    //play anim dead
-                    Destroy(this.gameObject); //즉시 삭제
-                }
-            }
-
-        }
-
-    }
-
-    IEnumerator hitting_delay()
-    {
-        agent.GetComponent<CapsuleCollider>().isTrigger = true; // <= 스켈레톤 전용
-        yield return new WaitForSeconds(3f);
-        enemy_state = State.Run;
-        agent.GetComponent<CapsuleCollider>().isTrigger = false;
-        StopCoroutine(hitting_delay());
-    }
+    
 }
