a9a01379df120e52d72293c8c676fa0a9ed1d989
ppic0423 <41542733+ppic0423@users.noreply.github.com>
2023-07-06 14:46:30 +0900

    플레이어 스테이트

---
+++ SummerPj/Assets/Scripts/PlayerState.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour
+{
+    public enum State
+    {
+        Idle, Walk, Sprint,
+        Jump,
+        WeakAttack_1, WeakAttack_2, WeakAttack_3 ,StrongAttack, JumpAttack, 
+        Heal, Heal_Walk,
+        Dead
+    }
+
+}
