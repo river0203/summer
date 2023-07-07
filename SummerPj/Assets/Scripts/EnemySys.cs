0ca91f0a86f043c58231a258ae2e16735c940928
river0203 <aspp3666@gmail.com>
2023-07-07 23:04:10 +0900

    dead

--- SummerPj/Assets/Scripts/EnemySys.cs
+++ SummerPj/Assets/Scripts/EnemySys.cs
@@ -1,19 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 using static LivingEntity;
 
 public class EnemySys : MonoBehaviour
 {
-    State _sysState;
+    LivingEntity.State _sysState;
     Transform _player;
     NavMeshAgent _sysAgent;
+    Rigidbody _sysRigid;
+
+    [SerializeField]
+    float _startingHP;
+    [SerializeField]
+    float _damage;
+
+    float _hp;
     
     void Start()
     {
         _player = GameObject.FindWithTag("Player").GetComponent<Transform>();
         _sysAgent = gameObject.GetComponent<NavMeshAgent>();
+        _startingHP = _hp;
+        _sysState = gameObject.GetComponent<LivingEntity.State>(); 
     }
 
     private void BossMove()
@@ -25,5 +36,33 @@ public class EnemySys : MonoBehaviour
     void Update()
     {
         BossMove();
+    }
+
+    void freeze_velocity()
+    {
+        _sysRigid.velocity = Vector3.zero;
+        _sysRigid.angularVelocity = Vector3.zero;
+
+    }
+
+    private void FixedUpdate()
+    {
+        freeze_velocity();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.CompareTag("Weapon"))
+        {
+            _hp -= _damage;
+            _sysState = LivingEntity.State.Stage2;
+
+            if(_hp <= 0)
+            {
+                _sysState = LivingEntity.State.Dead;
+                Destroy(this.gameObject);
+            }
+        }
+
     }
 }
