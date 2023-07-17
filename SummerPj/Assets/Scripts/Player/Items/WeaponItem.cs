58e8d4a41a0a6481d897831b135037866c9437b2
ppic0423 <41542733+ppic0423@users.noreply.github.com>
2023-07-17 16:50:57 +0900

    PT13 :: 무기 Idle 애니메이션

--- SummerPj/Assets/Scripts/Player/Items/WeaponItem.cs
+++ SummerPj/Assets/Scripts/Player/Items/WeaponItem.cs
@@ -8,6 +8,10 @@ public class WeaponItem : Item
     public GameObject _modelPrefabe;
     public bool _isUnarmed;
 
+    [Header("Idle Animations")]
+    public string right_hand_idle;
+    public string left_hand_idle;
+
     [Header("One Handed Attack Animations")]
     public string OH_Light_Attack_1;
     public string OH_Light_Attack_2;
