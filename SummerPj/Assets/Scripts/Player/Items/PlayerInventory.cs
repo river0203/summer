157f5894de9af98edaabf971947bdd3480a51c31
jungminna03 <jungminna03@gmail.com>
2023-07-24 10:56:28 +0900

    퀵슬롯 시스템 리펙토링

--- SummerPj/Assets/Scripts/Player/Items/PlayerInventory.cs
+++ SummerPj/Assets/Scripts/Player/Items/PlayerInventory.cs
@@ -12,6 +12,7 @@ public class PlayerInventory : MonoBehaviour
 
     public WeaponItem _unarmedWeapon;
 
+    public int _maxInventorySlotCount = 4;
     public WeaponItem[] _weaponsInRightHandSlots = new WeaponItem[1];
     public WeaponItem[] _weaponsInLeftHandSlots = new WeaponItem[1];
 
@@ -35,28 +36,17 @@ public class PlayerInventory : MonoBehaviour
 
     public void ChangeRightWeapon()
     {
-        _currentRightWeaponIndex = _currentRightWeaponIndex + 1;
+        if (_currentRightWeaponIndex < _maxInventorySlotCount - 1 && _currentRightWeaponIndex < _weaponsInRightHandSlots.Length - 1)
+        {
+            _currentRightWeaponIndex++;
 
-        if (_currentRightWeaponIndex == 0 && _weaponsInRightHandSlots[0] != null)
-        {
-            _rightWeapon = _weaponsInRightHandSlots[_currentRightWeaponIndex];
-            _weaponSlotManager.LoadWeaponOnSlot(_weaponsInRightHandSlots[_currentRightWeaponIndex], false);
-        }
-        else if (_currentRightWeaponIndex == 0 && _weaponsInRightHandSlots[0] == null)
-        {
-            _currentRightWeaponIndex += 1;
-        }
-        else if (_currentRightWeaponIndex == 1 && _weaponsInRightHandSlots[1] != null)
-        {
-            _rightWeapon = _weaponsInRightHandSlots[_currentRightWeaponIndex];
-            _weaponSlotManager.LoadWeaponOnSlot(_weaponsInRightHandSlots[_currentRightWeaponIndex], false);
+            if (_weaponsInRightHandSlots[_currentRightWeaponIndex] != null)
+            {
+                _rightWeapon = _weaponsInRightHandSlots[_currentRightWeaponIndex];
+                _weaponSlotManager.LoadWeaponOnSlot(_weaponsInRightHandSlots[_currentRightWeaponIndex], false);
+            }
         }
         else
-        {
-            _currentRightWeaponIndex = _currentRightWeaponIndex + 1;
-        }
-
-        if(_currentRightWeaponIndex > _weaponsInRightHandSlots.Length - 1) 
         {
             _currentRightWeaponIndex = -1;
             _rightWeapon = _unarmedWeapon;
@@ -66,25 +56,17 @@ public class PlayerInventory : MonoBehaviour
 
     public void ChangeLeftWeapon()
     {
-        _currentLeftWeaponIndex = _currentLeftWeaponIndex + 1;
+        if (_currentLeftWeaponIndex < _maxInventorySlotCount - 1 && _currentLeftWeaponIndex < _weaponsInLeftHandSlots.Length - 1)
+        {
+            _currentLeftWeaponIndex++;
 
-        if (_currentLeftWeaponIndex > _weaponsInLeftHandSlots.Length - 1)
-        {
-            _currentLeftWeaponIndex = -1;
-            _leftWeapon = _unarmedWeapon;
-            _weaponSlotManager.LoadWeaponOnSlot(_unarmedWeapon, true);
-        }
-        else if (_weaponsInLeftHandSlots[_currentLeftWeaponIndex] != null)
-        {
-            _leftWeapon = _weaponsInLeftHandSlots[_currentLeftWeaponIndex];
-            _weaponSlotManager.LoadWeaponOnSlot(_weaponsInLeftHandSlots[_currentLeftWeaponIndex], true);
+            if (_weaponsInLeftHandSlots[_currentLeftWeaponIndex] != null)
+            {
+                _leftWeapon = _weaponsInLeftHandSlots[_currentLeftWeaponIndex];
+                _weaponSlotManager.LoadWeaponOnSlot(_weaponsInLeftHandSlots[_currentLeftWeaponIndex], true);
+            }
         }
         else
-        {
-            _currentLeftWeaponIndex = _currentLeftWeaponIndex + 1;
-        }
-
-        if (_currentLeftWeaponIndex > _weaponsInLeftHandSlots.Length - 1)
         {
             _currentLeftWeaponIndex = -1;
             _leftWeapon = _unarmedWeapon;
