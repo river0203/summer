a963c5ec4621f934993fabd1cf2dcb7778bff24c
river0203 <aspp3666@gmail.com>
2023-08-04 17:36:46 +0900

    잡몹 존재시 회복

---
+++ SummerPj/Assets/Scripts/Enemys/State/RebirthState.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RebirthState : State
+{
+    private IdleState IdleState;
+    private LayerMask _detectionLayer;
+    private CharacterStatsManager _characterStatsManager;
+    public override State Tick(EnemyManager enemyManger, EnemyStats enemyStats, EnemyAnimatorManager enemyAnimatorManger)
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, enemyManger.detectionRadius, _detectionLayer);
+
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            // CharacterStats를 가져옴 (캐릭터인지 아닌지 판별)
+            MobState _mobState = colliders[i].transform.GetComponent<MobState>();
+
+            if(_mobState != null && _characterStatsManager._isDead == true)
+            {
+                _characterStatsManager._currentHealth += 50;
+                return IdleState;
+            }
+            else
+            {
+                return this;
+            }
+
+        }
+
+        return IdleState;
+    }
+}
