6ef48818a596351d571bde2ad14c1f4cfd1df580
river0203 <aspp3666@gmail.com>
2023-08-06 23:18:45 +0900

    잠복 애니메이션 설정

--- SummerPj/Assets/Scripts/Enemys/EventColliderBeginBossFight.cs
+++ SummerPj/Assets/Scripts/Enemys/EventColliderBeginBossFight.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class EventColliderBeginBossFight : MonoBehaviour
 {
     WorldEventManager _worldEventManager;
+    EnemyAnimatorManager _enemyAnimatorManger;
+
+    public bool _inAnim;
 
     private void Awake()
     {
         _worldEventManager = FindObjectOfType<WorldEventManager>();
+        _enemyAnimatorManger = FindObjectOfType<EnemyAnimatorManager>();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -18,5 +22,7 @@ public class EventColliderBeginBossFight : MonoBehaviour
             _worldEventManager.ActivateBossFight();
         }*/
         _worldEventManager.ActivateBossFight();
+        _inAnim = true;
     }
+
 }
