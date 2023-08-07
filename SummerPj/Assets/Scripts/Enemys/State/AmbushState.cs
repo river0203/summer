3f4f5b589b968325d51b0dfa5ebde05a5b172235
river0203 <aspp3666@gmail.com>
2023-08-07 14:19:09 +0900

    수치 조정

--- SummerPj/Assets/Scripts/Enemys/State/AmbushState.cs
+++ SummerPj/Assets/Scripts/Enemys/State/AmbushState.cs
@@ -57,7 +57,7 @@ public class AmbushState : State
 
     IEnumerator WaitSeconds()
     {
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(3);
 
         isSleeping = false; // 잠 상태 해제
     }
