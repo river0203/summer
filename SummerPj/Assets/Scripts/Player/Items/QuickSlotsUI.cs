ffbcd7588afafca86b63077422670a0329858520
ppic0423 <41542733+ppic0423@users.noreply.github.com>
2023-08-11 11:11:27 +0900

    지형지물 콜라이더 추가

--- SummerPj/Assets/Scripts/Player/Items/QuickSlotsUI.cs
+++ SummerPj/Assets/Scripts/Player/Items/QuickSlotsUI.cs
@@ -5,7 +5,6 @@ using UnityEngine.UI;
 
 public class QuickSlotsUI : MonoBehaviour
 {
-    public Image _magicIcon;
     public Image _rightWeaponIcon;
 
     public void UpdateWeaponQuickSlotsUI(WeaponItem weapon, SpellItem spell)
@@ -13,16 +12,12 @@ public class QuickSlotsUI : MonoBehaviour
             if (weapon.itemIcon != null)
             {
                 _rightWeaponIcon.sprite = weapon.itemIcon;
-                _magicIcon.sprite = spell.itemIcon;
                 _rightWeaponIcon.enabled = true;
-                _magicIcon.enabled = true;
         }
             else
             {
                 _rightWeaponIcon.sprite = null;
-                _magicIcon.sprite = null;
                 _rightWeaponIcon.enabled = false;
-                _magicIcon.enabled = false;
             }
 
     }
