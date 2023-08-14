fd1c6b84acbc1c256d53b79e82edfa64c21853fc
ppic0423 <41542733+ppic0423@users.noreply.github.com>
2023-08-14 15:14:14 +0900

    잡몹

---
+++ SummerPj/Assets/Resources/Mob/MobHealthBar.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MobHealthBar : MonoBehaviour
+{
+    public Slider _slider;
+    float _timeUntillBarIsHidden = 0;
+
+    public void SetHealth(int health)
+    {
+        _slider.value = health;
+        _timeUntillBarIsHidden = 3;
+    }
+
+    public void SetMaXHealth(int maxHealth)
+    {
+        _slider.maxValue = maxHealth;
+        _slider.value = maxHealth;
+    }
+
+    private void Update()
+    {
+        _timeUntillBarIsHidden -= Time.deltaTime;
+
+        if (_slider != null)
+        {
+            if (_timeUntillBarIsHidden <= 0)
+            {
+                _timeUntillBarIsHidden = 0;
+                _slider.gameObject.SetActive(false);
+            }
+            else
+            {
+                if (!_slider.gameObject.activeInHierarchy)
+                {
+                    _slider.gameObject.SetActive(true);
+                }
+            }
+
+            if (_slider.value <= 0)
+            {
+                Destroy(_slider.gameObject);
+            }
+        }
+    }
+}
--- SummerPj/Assets/Scripts/DamageCollider.cs
+++ SummerPj/Assets/Scripts/DamageCollider.cs
@@ -80,6 +80,21 @@ public class DamageCollider : MonoBehaviour
                 _enemyEffectsManager.PlayBloodSplatterFX(contactPoint);
             }
         }
+
+        if (collision.tag == "Mobs")
+        {
+            MobState mobStats = collision.GetComponent<MobState>();
+            CharacterEffectsManager _characterEffectsManager = collision.GetComponent<CharacterEffectsManager>();
+
+            Vector3 contactPoint = collision.gameObject.GetComponent<Collider>().ClosestPointOnBounds(transform.position);
+
+            if (mobStats != null)
+            {
+                mobStats.TakeDamage(_currentWeaponDamage);
+                _characterEffectsManager.PlayBloodSplatterFX(contactPoint);
+            }
+        }
+
     }
     public void EnableDamagecollider()
     {
