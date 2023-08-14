b6e6c656e6d5ea02d4386ec979bdfa405d5ec6e5
river0203 <aspp3666@gmail.com>
2023-08-14 16:46:23 +0900

    프리펩

--- SummerPj/Assets/Resources/Mob/MobState.cs
+++ SummerPj/Assets/Resources/Mob/MobState.cs
@@ -16,6 +16,8 @@ public class MobState : CharacterStatsManager
     public MobHealthBar mobHealthBar;
 
     int _damage = 25;
+    bool _startIdle = false;
+    bool _canAttack = false;
     public float _rotationSpeed = 3;
 
     enum State
@@ -35,50 +37,80 @@ public class MobState : CharacterStatsManager
 
     void Start()
     {
+        anim.Play("getup");
         _maxHealth = 50;
         _currentHealth = _maxHealth;
         mobHealthBar.SetMaXHealth(_maxHealth);
-        state = State.Idle;
+        if(_startIdle )
+        {
+           // state = State.Idle;
+        }
     }
 
+    public void Playgetup()
+    {
+        _startIdle = false;
+    }
+
+    public void EndGetup()
+    {
+        _startIdle = true;
+    }
     void Update()
     {
-        if (state == State.Idle)
+        if(_startIdle)
         {
-            UpdateIdle();
-        }
-        else if (state == State.Run)
-        {
-            UpdateRun();
-        }
-        else if (state == State.Attack)
-        {
-            UpdateAttack();
+            if (state == State.Idle)
+            {
+                UpdateIdle();
+            }
+            else if (state == State.Run)
+            {
+                UpdateRun();
+            }
+            else if (state == State.Attack)
+            {
+                UpdateAttack();
+            }
         }
     }
 
     private void UpdateAttack()
     {
         agent.speed = 0;
-        //anim.Play("attack02");
+        if(_canAttack)
+        {
+            anim.Play("attack02");
+        }
+        StartCoroutine(HitDelay());
         float distance = Vector3.Distance(transform.position, target.transform.position);
         if (distance > 2)
         {
             state = State.Run;
-            anim.Play("walk");
         }
+    }
+
+    public void PlayAttack()
+    {
+        _canAttack = false;
+    }
+
+    public void EndAttack()
+    {
+        _canAttack = true;
     }
 
     private void UpdateRun()
     {
         float distance = Vector3.Distance(transform.position, target.transform.position);
-        
-        if(_currentHealth > 0)
+
+        anim.Play("walk");
+        if (_currentHealth > 0)
         {
             if (distance <= 2)
             {
                 state = State.Attack;
-                anim.Play("attack02");
+                _canAttack = true;
             }
         }
         else
@@ -97,7 +129,6 @@ public class MobState : CharacterStatsManager
         if (target != null)
         {
             state = State.Run;
-            anim.Play("walk");
         }
     }
 
